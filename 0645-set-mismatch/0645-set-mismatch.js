1353218d44306725918e9524c23be40e6d4aebb7
piyush <piyushkochar909@gmail.com>
2026-01-13 17:05:49 +0530

    0645-SET-MISMATCH Stats: Time: 16 ms (39.22%), Space: 64.8 MB (7.45%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -1,25 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 var findErrorNums = function(nums) {
+    let n = nums.length;
+    let map = new Map();
     let repeating = 0;
     let missing = 0;
-    let n = nums.length;
-
-    nums.sort((a, b) => a - b);
-
-    for (let i = 0; i < n - 1; i++) {
-        if (nums[i] === nums[i + 1]) {
-            repeating = nums[i];
-        } 
-        else if (nums[i + 1] !== nums[i] + 1) {
-            missing = nums[i] + 1;
+    for(let num of nums){
+        map.set(num , (map.get(num) || 0) + 1);
+    }
+    for(let i = 1; i <= n; i++){
+        if(!map.has(i)){
+            missing = i;
+        } else if(map.get(i) == 2){
+            repeating = i;
         }
     }
-
-    if (nums[0] !== 1) {
-        missing = 1;
-    }
-    if (nums[n - 1] !== n) {
-        missing = n;
-    }
-
-    return [repeating, missing];
+    return [repeating , missing];
 };
