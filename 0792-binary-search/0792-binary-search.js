64496dbd5a355b51f3c0d26c64279d8e30316d18
piyush <piyushkochar909@gmail.com>
2026-01-14 22:52:49 +0530

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 58.6 MB (16.33%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -1,14 +1,51 @@
-var search = function(nums, target) {
-    let low = 0;
-    let high = nums.length - 1;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 
-    while (low <= high) {
+ //recursive way to approach binary search
+var search = function(nums, target) {
+    function binary(low, high) {
+        if (low > high) return -1;
+
         let mid = Math.floor((low + high) / 2);
 
         if (nums[mid] === target) return mid;
-        else if (target > nums[mid]) low = mid + 1;
-        else high = mid - 1;
+        else if (nums[mid] > target) {
+            return binary(low, mid - 1);
+        } else {
+            return binary(mid + 1, high);
+        }
     }
 
-    return -1; 
+    return binary(0, nums.length - 1);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//binary search using iterations
+// var search = function(nums, target) {
+//     let low = 0;
+//     let high = nums.length - 1;
+//     while (low <= high) {
+//         let mid = Math.floor((low + high) / 2);
+//         if (nums[mid] === target) return mid;
+//         else if (target > nums[mid]) low = mid + 1;
+//         else high = mid - 1;
+//     }
+//     return -1; 
+// };
