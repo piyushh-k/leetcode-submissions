25e8da05c2ff98e1e258c1d72aa57d8c8d1cd137
piyush <piyushkochar909@gmail.com>
2026-01-15 22:43:50 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 53.7 MB (97.22%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -1,13 +1,36 @@
-//using linear search
 var searchRange = function(nums, target) {
+    let n = nums.length;
     let start = -1;
     let end = -1;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            if (start === -1) start = i;
-            end = i;
+    // find start
+    let low = 0, high = n - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[mid] === target) {
+            start = mid;
+            high = mid - 1;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
+
+    // find end
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[mid] === target) {
+            end = mid;
+            low = mid + 1;
+        } else if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
     return [start, end];
 };
