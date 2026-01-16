ac0ad6f68d4949a61bd61d45208c5405f4707fa8
sid <sidhantgupta2005@gmail.com>
2026-01-17 03:24:42 +0530

    Added Java code  in 0540-single-element-in-a-sorted-array.js

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -34,3 +34,21 @@ var singleNonDuplicate = function(nums) {
 //         }
 //     }
 // };
+
+//Java Linear search
+//Linear Search.
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         int n = nums.length;
+//         for (int i = 0 ; i<n-1; i=i+2){
+//             if (nums[i] == nums[i+1]){
+//                 continue;
+//             }
+//             if (nums[i] != nums[i+1]){
+//                 return nums[i];
+                
+//             }
+//         }
+//         return nums[n-1];  //return the last elem
+//     }
+// }
