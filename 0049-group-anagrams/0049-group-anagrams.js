abaf8a61dfefdca73c5885e50ec37175b94ac302
piyush <piyushkochar909@gmail.com>
2026-01-18 00:20:28 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 29 ms (71.67%), Space: 68.7 MB (39.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,15 +2,49 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+/*This approach is aa bit difficult intutively to come up on my own 
+first make a map that will store our anagrams on diff buckets 
+now i will loop through the array , for each string i will create a new 26 length array that will store the frequency of each lil alphabet in that array
+lets say "bad" represents the state : 110100000000000000 
+now I will store this frequency string in the map
+if any other anagram comes up of this word it will have the same freq string hence i can then append it to the same bucket that "bad" is occupying
+*/
+
+var groupAnagrams = function (strs) {
     let map = new Map();
-    let arr = [];
-    for(let str of strs){
-        let x = str.split("").sort().join("");
-        if(!map.has(x)){
-            map.set(x , []);
+    for (let word of strs) {
+        let arr = new Array(26).fill(0);
+        for (let c of word) {
+            arr[c.charCodeAt(0) - 97]++;
         }
-        map.get(x).push(str);
+        let code = arr.join("-"); /*very important to avoid collision*/
+
+        if (!map.has(code)) {
+            map.set(code, []);
+        }
+        map.get(code).push(word)
     }
     return Array.from(map.values());
 };
+// joining like ("") will fail in cases like :
+// 1, 10, 1].join("-")   "1-10-1"
+// [11, 0, 1].join("-")  "11-0-1"
+
+
+
+
+
+
+//  using sorting
+// var groupAnagrams = function(strs) {
+//     let map = new Map();
+//     let arr = [];
+//     for(let str of strs){
+//         let x = str.split("").sort().join("");
+//         if(!map.has(x)){
+//             map.set(x , []);
+//         }
+//         map.get(x).push(str);
+//     }
+//     return Array.from(map.values());
+// };
