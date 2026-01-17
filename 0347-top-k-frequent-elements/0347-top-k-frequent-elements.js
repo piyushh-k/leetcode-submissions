76587d2e8a6507ccc95d170bdbc59b52c80b94e6
piyush <piyushkochar909@gmail.com>
2026-01-18 04:00:12 +0530

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 24 ms (13.73%), Space: 65.2 MB (5.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -1,14 +1,40 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
+
 var topKFrequent = function(nums, k) {
     let n = nums.length;
+
     let map = new Map();
-    for(let i = 0; i < n; i++){
-        map.set(nums[i] , (map.get(nums[i]) || 0) + 1);
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
     }
-    const arr = Array.from(map.entries()).sort((a,b) => b[1] - a[1]);
-    return arr.slice(0, k).map((t) => t[0]);
+
+    let hashArr = new Array(n + 1);
+    for (let i = 0; i <= n; i++) {
+        hashArr[i] = [];
+    }
+
+    for (let [key, val] of map) {
+        hashArr[val].push(key);
+    }
+
+    let result = [];
+    for (let i = n; i >= 0 && result.length < k; i--) {
+        for (let num of hashArr[i]) {
+            result.push(num);
+            if (result.length === k) break;
+        }
+    }
+    return result;
 };
+
+
+
+
+// var topKFrequent = function(nums, k) {
+//     let n = nums.length;
+//     let map = new Map();
+//     for(let i = 0; i < n; i++){
+//         map.set(nums[i] , (map.get(nums[i]) || 0) + 1);
+//     }
+//     const arr = Array.from(map.entries()).sort((a,b) => b[1] - a[1]);
+//     return arr.slice(0, k).map((t) => t[0]);
+// };
