ccffdfd52423bf24dc4aa74aa14763ca4349a920
piyush <piyushkochar909@gmail.com>
2026-01-23 16:34:09 +0530

    0015-3SUM Stats: Time: 38 ms (45.63%), Space: 72.7 MB (46.27%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,26 +4,26 @@
  */
 var threeSum = function(nums) {
     let n = nums.length;
-    let arr = [];
     nums.sort((a,b) => a-b);
-    for(let i = 0; i < n; i++){
-        if( i > 0 && nums[i] == nums[i-1]) continue;
-        let j = i+1;
-        let k = n-1;
-        while(j < k){
-            let sum = nums[i] + nums[j] + nums[k];
+    let res = [];
+    for(let i = 0; i < n-2; i++){
+        if(i > 0 && nums[i] === nums[i-1]) continue;
+        let low = i+1;
+        let high = n-1;
+        while(low < high){
+            let sum = nums[i] + nums[low] + nums[high];
             if(sum === 0){
-                arr.push([nums[i] , nums[j] , nums[k]]);
-                j++;
-                k--;
-                while( j < k && nums[j] == nums[j-1]){j++};
-                while( j < k && nums[k] == nums[k+1]){k--};
-            }else if(sum > 0){
-                k--;
-            } else {
-                j++;
+                res.push([nums[i] , nums[low] , nums[high]]);
+                low++;
+                high--;
+                while(low < high && nums[low] == nums[low-1]) low++;
+                while(low < high && nums[high] == nums[high+1]) high--;
+            }else if(sum > 0) {
+                high--;
+            }else{
+                low++;
             }
         }
     }
-    return arr;
+    return res;
 };
