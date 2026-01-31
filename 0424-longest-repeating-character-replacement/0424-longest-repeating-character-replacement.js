b09e1aca2fb02dcd2661be4f8ee4f647f13364d8
piyush <piyushkochar909@gmail.com>
2026-01-31 15:57:23 +0530

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 30 ms (30.27%), Space: 60.6 MB (16.53%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -1,22 +1,28 @@
-var characterReplacement = function (s, k) {
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
     let n = s.length;
-    let low = 0;
     let map = new Map();
+    let low = 0;
+    let high = 0;
     let maxWindow = 0;
-
-    for (let high = 0; high < n; high++) {
-        map.set(s[high], (map.get(s[high]) || 0) + 1);
+    while(high < n){
+        map.set(s[high] , (map.get(s[high]) ||  0) + 1);
         let maxVal = 0;
-        for (let val of map.values()) {
-            maxVal = Math.max(maxVal, val);
+        for(let val of map.values()){
+            maxVal = Math.max(maxVal , val);
         }
-        while (high - low + 1 - maxVal > k) {
-            map.set(s[low], map.get(s[low]) - 1);
+        let currentLen = high-low+1;
+        while(currentLen - maxVal > k){
+            map.set(s[low] , map.get(s[low]) - 1);
             low++;
+            currentLen--;
         }
-
-        maxWindow = Math.max(maxWindow, high - low + 1);
+        maxWindow = Math.max(maxWindow , currentLen);
+        high++;
     }
-
     return maxWindow;
 };
