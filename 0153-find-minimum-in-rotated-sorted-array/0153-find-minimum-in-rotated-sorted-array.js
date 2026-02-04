3f20437cc21c688429adb479a0ebf09cb102e323
piyush <piyushkochar909@gmail.com>
2026-02-04 21:50:01 +0530

    0153-FIND-MINIMUM-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 54.2 MB (27.58%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -8,13 +8,13 @@ var findMin = function(nums) {
     let high = n-1;
     let min = +Infinity;
     while(low <= high){
-        let mid = Math.floor((low+high)/2);
-        if(nums[mid] <= nums[high]){
-            min = Math.min(nums[mid],min);
-            high = mid-1
-        } else if(nums[low] <= nums[mid]){
-            min = Math.min(nums[low], min);
+        let mid = Math.floor((low+high)/2)
+        if(nums[low] <= nums[mid]){
+            min = Math.min(min , nums[low]);
             low = mid+1;
+        }else if(nums[mid] <= nums[high]){
+            min = Math.min(min , nums[mid]);
+            high = mid-1;
         }
     }
     return min;
