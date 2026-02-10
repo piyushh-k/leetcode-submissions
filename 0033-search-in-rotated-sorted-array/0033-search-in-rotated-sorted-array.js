71c4da1faacf79e088d508b275718c3521ab403f
piyush <piyushkochar909@gmail.com>
2026-02-10 23:36:59 +0530

    0033-SEARCH-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 54.2 MB (35.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -9,20 +9,18 @@ var search = function(nums, target) {
     let high = n-1;
     while(low <= high){
         let mid = Math.floor((low+high)/2);
-        if(nums[mid] === target) return mid;
-        //left half is sorted
-        else if(nums[low] <= nums[mid]){
+        if (nums[mid] === target) return mid;
+        if(nums[low] <= nums[mid]){
             if(nums[low] <= target && target <= nums[mid]){
                 high = mid-1;
-            } else{
-                low = mid+1;
+            }else {
+                low = mid + 1;
             }
-        //right half is sorted
-        } else if(nums[mid] <= nums[high]){
+        } else {
              if(nums[mid] <= target && target <= nums[high]){
-                low = mid+1;
-            } else{
-                high = mid-1;
+                low = mid+1
+            }else {
+                high = mid - 1;
             }
         }
     }
